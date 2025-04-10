465f2a3f63e6172b16685ac7ade0f07b506d1e45
karl <enriquezkharl14@gmail.com>
2025-04-10 23:07:07 +0800

    [Feat] Util random strings renerator

---
+++ internal/util/random.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.NewSource(time.Now().UnixNano())
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+
+	k := len(alphabet)
+
+	for range n {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomEmail() string {
+	return fmt.Sprintf("%s@mail.com", RandomString(6))
+}
