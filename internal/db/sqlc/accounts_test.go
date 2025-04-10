32a3eab2329979904bc11702415bd1b7240fbf7a
karl <enriquezkharl14@gmail.com>
2025-04-10 23:15:56 +0800

    [Test] Delete account

--- internal/db/sqlc/accounts_test.go
+++ internal/db/sqlc/accounts_test.go
@@ -2,20 +2,23 @@ package db
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/kharljhon14/porma-pro-server/internal/util"
 	"github.com/stretchr/testify/require"
 )
 
 func createTestAccount(t *testing.T) Account {
-	hashedPassword, err := util.HashedPassword("Password")
+	hashedPassword, err := util.HashedPassword(util.RandomString(8))
 	require.NoError(t, err)
 
 	args := CreateAccountParams{
-		Email:        "enriquezkharl14@gmail.com",
+		Email:        util.RandomEmail(),
 		PasswordHash: hashedPassword,
-		FullName:     "Kharl Jhon Rhane Enriquez",
+		FullName:     util.RandomString(10),
 	}
 
 	account, err := testStore.CreateAccount(context.Background(), args)
@@ -35,4 +38,30 @@ func createTestAccount(t *testing.T) Account {
 
 func TestCreateAccount(t *testing.T) {
 	createTestAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account := createTestAccount(t)
+
+	account2, err := testStore.GetAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account.FullName, account2.FullName)
+	require.Equal(t, account.Email, account2.Email)
+	require.Equal(t, account.PasswordHash, account2.PasswordHash)
+	require.WithinDuration(t, account.CreatedAt.Time, account2.CreatedAt.Time, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account := createTestAccount(t)
+
+	err := testStore.DeleteAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+
+	account2, err := testStore.GetAccount(context.Background(), account.ID)
+	require.Error(t, err)
+	errorMessage := fmt.Errorf("sql: %s", err)
+	require.EqualError(t, errorMessage, sql.ErrNoRows.Error())
+	require.Empty(t, account2)
 }
