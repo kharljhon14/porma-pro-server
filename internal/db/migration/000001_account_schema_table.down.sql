4c0868fe30247f376880a66f72b7019d1d53c0f8
karl <enriquezkharl14@gmail.com>
2025-04-10 21:34:07 +0800

    [Feat] Init migration

--- internal/db/migration/000001_account_schema_table.down.sql
+++ internal/db/migration/000001_account_schema_table.down.sql
@@ -1,11 +1,1 @@
-CREATE TABLE "accounts"(
-    "id" bigserial PRIMARY KEY,
-    "email" varchar UNIQUE NOT NULL,
-    "password_hash" varchar,
-    "sso_provider" varchar,
-    "sso_provider_id" varchar,
-    "full_name" varchar NOT NULL,
-    "created_at" timestamp NOT NULL DEFAULT (now()),
-    "updated_at" timestamp NOT NULL DEFAULT (now()),
-    "is_verified" boolean NOT NULL DEFAULT False,
-);
+DROP TABLE IF EXISTS accounts;
