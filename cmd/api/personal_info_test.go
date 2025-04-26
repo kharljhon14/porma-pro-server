7d46578ee63c06d9046f9af3c41d05ef28705d2b
karl <enriquezkharl14@gmail.com>
2025-04-26 17:15:31 +0800

    [Test] Test Update personal info handler

--- cmd/api/personal_info_test.go
+++ cmd/api/personal_info_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/jackc/pgx/v5/pgtype"
 	mock_sqlc "github.com/kharljhon14/porma-pro-server/internal/db/mock"
 	db "github.com/kharljhon14/porma-pro-server/internal/db/sqlc"
 	"github.com/kharljhon14/porma-pro-server/internal/util"
@@ -230,6 +231,128 @@ func TestGetPersonalInfo(t *testing.T) {
 			require.NoError(t, err)
 
 			server.router.ServeHTTP(recorder, req)
+			tc.checkResponse(t, recorder)
+		})
+	}
+}
+
+func TestUpdatePersonalInfo(t *testing.T) {
+	args := updatePersonalInfoRequest{
+		FullName:    util.RandomString(12),
+		Email:       util.RandomEmail(),
+		LinkedInURL: "https://google.com",
+		PersonalURL: "https://google.com",
+		PhoneNumber: "+639456543438",
+		Country:     "Philippines",
+		State:       "Bataan",
+		City:        "Orion",
+	}
+	uriID := int64(1)
+
+	personalInfo := db.PersonalInfo{
+		ID:        1,
+		AccountID: 1,
+		FullName:  util.RandomString(12),
+		Email:     util.RandomEmail(),
+		LinkedinUrl: pgtype.Text{
+			String: "https://google.com",
+			Valid:  true,
+		},
+		PersonalUrl: pgtype.Text{
+
+			String: "https://google.com",
+			Valid:  true,
+		},
+		PhoneNumber: "+639456543438",
+		Country:     "Philippines",
+		State:       "Bataan",
+		City:        "Orion",
+	}
+
+	testCases := []struct {
+		name          string
+		args          updatePersonalInfoRequest
+		uri           int64
+		buildStubs    func(store *mock_sqlc.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "Ok",
+			args: args,
+			uri:  uriID,
+			buildStubs: func(store *mock_sqlc.MockStore) {
+				store.
+					EXPECT().
+					UpdatePersonalInfo(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(personalInfo, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+
+				data, err := io.ReadAll(recorder.Body)
+				require.NoError(t, err)
+
+				var gotPersonalInfo db.PersonalInfo
+				err = json.Unmarshal(data, &gotPersonalInfo)
+				require.NoError(t, err)
+
+				require.NotEmpty(t, personalInfo)
+			},
+		},
+		{
+			name: "BadRequest",
+			args: args,
+			uri:  0,
+			buildStubs: func(store *mock_sqlc.MockStore) {
+				store.
+					EXPECT().
+					UpdatePersonalInfo(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "InternalError",
+			args: args,
+			uri:  uriID,
+			buildStubs: func(store *mock_sqlc.MockStore) {
+				store.
+					EXPECT().
+					UpdatePersonalInfo(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.PersonalInfo{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mock_sqlc.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := newTestingServer(t, store)
+
+			recorder := httptest.NewRecorder()
+
+			js, err := json.Marshal(tc.args)
+			require.NoError(t, err)
+
+			url := fmt.Sprintf("/personal-info/%d", tc.uri)
+			request, err := http.NewRequest(http.MethodPatch, url, bytes.NewBuffer(js))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+
 			tc.checkResponse(t, recorder)
 		})
 	}
