42d8edb51410ba4cf6e4dbcacd0c9419e8e9554c
karl <enriquezkharl14@gmail.com>
2025-04-27 22:51:10 +0800

    [Test] Update and delete summary api handlers

--- cmd/api/summary_test.go
+++ cmd/api/summary_test.go
@@ -217,3 +217,182 @@ func TestGetSummary(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdateSummary(t *testing.T) {
+	args := db.UpdateSummaryParams{
+		ID:      1,
+		Summary: util.RandomString(1000),
+	}
+
+	summary := db.Summary{
+		ID:        args.ID,
+		AccountID: 1,
+		Summary:   args.Summary,
+	}
+
+	testCases := []struct {
+		name          string
+		args          db.UpdateSummaryParams
+		buildStubs    func(store *mock_sqlc.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "Ok",
+			args: args,
+			buildStubs: func(store *mock_sqlc.MockStore) {
+				store.
+					EXPECT().
+					UpdateSummary(gomock.Any(), gomock.Eq(args)).
+					Times(1).
+					Return(summary, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+
+				data, err := io.ReadAll(recorder.Body)
+				require.NoError(t, err)
+
+				var gotSummary db.Summary
+				err = json.Unmarshal(data, &gotSummary)
+				require.NoError(t, err)
+
+				require.Equal(t, summary, gotSummary)
+			},
+		},
+		{
+			name: "BadRequest",
+			args: db.UpdateSummaryParams{},
+			buildStubs: func(store *mock_sqlc.MockStore) {
+				store.
+					EXPECT().
+					UpdateSummary(gomock.Any(), gomock.Eq(db.UpdateSummaryParams{})).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "InternalError",
+			args: args,
+			buildStubs: func(store *mock_sqlc.MockStore) {
+				store.
+					EXPECT().
+					UpdateSummary(gomock.Any(), gomock.Eq(args)).
+					Times(1).
+					Return(db.Summary{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mock_sqlc.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := newTestingServer(t, store)
+
+			recorder := httptest.NewRecorder()
+
+			url := fmt.Sprintf("/summary/%d", tc.args.ID)
+
+			js, err := json.Marshal(tc.args)
+			require.NoError(t, err)
+
+			request, err := http.NewRequest(http.MethodPatch, url, bytes.NewBuffer(js))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+
+			tc.checkResponse(t, recorder)
+
+		})
+	}
+}
+
+func TestDeleteSummary(t *testing.T) {
+	id := int64(1)
+
+	testCases := []struct {
+		name          string
+		id            int64
+		buildStubs    func(store *mock_sqlc.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "Ok",
+			id:   id,
+			buildStubs: func(store *mock_sqlc.MockStore) {
+				store.
+					EXPECT().
+					DeleteSummary(gomock.Any(), gomock.Eq(id)).
+					Times(1).
+					Return(nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name: "BadRequest",
+			id:   0,
+			buildStubs: func(store *mock_sqlc.MockStore) {
+				store.
+					EXPECT().
+					DeleteSummary(gomock.Any(), gomock.Eq(0)).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "InternalError",
+			id:   id,
+			buildStubs: func(store *mock_sqlc.MockStore) {
+				store.
+					EXPECT().
+					DeleteSummary(gomock.Any(), gomock.Eq(id)).
+					Times(1).
+					Return(sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mock_sqlc.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := newTestingServer(t, store)
+
+			recorder := httptest.NewRecorder()
+
+			url := fmt.Sprintf("/summary/%d", tc.id)
+
+			request, err := http.NewRequest(http.MethodDelete, url, nil)
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+
+			tc.checkResponse(t, recorder)
+
+		})
+	}
+}
